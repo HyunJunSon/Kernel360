17f2077bc666b1fd769bea146e668aaf508710d2
HyunJunSon <jjang3a@gmail.com>
2023-09-27 18:57:34 +0900

    study add

---
+++ src/FindAPI.java
@@ -0,0 +1,8 @@
+public class FindAPI {
+    public static void main(String[] args) {
+        int a;
+        java.lang.String s = "Apple";
+        System.out.println("s = " + s);
+        System.out.println(s.length());
+    }
+}
