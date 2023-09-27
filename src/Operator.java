e9073a0533ea655f72f1da30a57fb6dcbce47ad6
HyunJunSon <jjang3a@gmail.com>
2023-09-27 17:02:00 +0900

    kernel360 precourse_suggest #1

---
+++ src/Operator.java
@@ -0,0 +1,13 @@
+public class Operator {
+    public static void main(String[] args) {
+        int kor, eng, tot;
+
+        kor = 100;
+        eng = 90;
+        tot = kor + eng;
+
+        System.out.printf("총점은 %d 입니다.", tot);
+
+
+    }
+}
